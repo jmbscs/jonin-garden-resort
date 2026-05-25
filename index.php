e2684ffe56d2579bcc1f5de9556d7fb238f27e42
jmbscs <voluntad.j.m.bscs@gmail.com>
2026-05-25 23:49:45 +0000

    Fix index redirect to not intercept PHP files

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php header("Location: /homepage_index.html"); exit;
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+if (strpos($uri, '.php') !== false || strpos($uri, '.css') !== false || strpos($uri, '.js') !== false || strpos($uri, '/images/') !== false) {
+    return false;
+}
+header('Location: /homepage_index.html');
+exit;
